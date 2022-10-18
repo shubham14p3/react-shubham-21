ccdaaef72b6ab544b3191f2cdb4a3fbf2c3a719b
Shubham Raj <shubham14p3@gmail.com>
2022-10-18 17:20:20 +0530

    Adding Files

---
+++ src/components/pages/index.js
@@ -0,0 +1,11 @@
+// import Home from './Home';
+// import Blog from './Blog';
+// import BlogSingle from './BlogSingle';
+
+// const routes = [
+//     { path: "/", component: Home},
+//     { path: "/blog", component: Blog},
+//     { path: "/blog-single", component: BlogSingle},
+// ]
+
+// export default routes;
