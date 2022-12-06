bd46e335092f02765ffdf3c6ecfd631e4d3e04e9
Shubham Raj <shubham14p3@gmail.com>
2022-12-06 20:01:24 +0530

    Adding new resume

--- src/components/layouts/home01/Specilizing.jsx
+++ src/components/layouts/home01/Specilizing.jsx
@@ -17,13 +17,13 @@ class Specilizing extends Component {
           title: "Role: Consultant",
           date: "Date: June 2022 - current",
           text01:
-            "★ Currently working on Financial Services domain for Discover Bank (Capgemini Client)",
+            "★ Currently working on the Financial Services domain for Discover Bank (Capgemini Client).",
           text02:
-            "★ Designed and developed over 50+ user-facing features via ReactJS and build front end libraries for continuous development",
+            "★ Designed and developed over 50+ user-facing features via React JS and build front-end libraries",
           text03:
-            "★ Worked alongside other developers to implement Restful APIs that enabled the internal team to increase the reporting speed by 35%.",
+            "★ Worked alongside other developers to implement Restful APIs that enabled the internal team to increase the loading speed by 35%, Converted over 30+ components to HOC and more.",
           text04:
-            "★ Tested and optimized 40+ React component for best performance across devices and browser.",
+            "★ Tested and optimized 140+ React components for better performance across devices and browsers. ",
         },
         {
           id: 2,
@@ -34,13 +34,13 @@ class Specilizing extends Component {
           title: "Role: Associate Consultant",
           date: "Date: May 2021 - May 2022",
           text01:
-            "★ Worked on Financial Services domain for Abu Dhabi Bank (Capgemini Client). ",
+            "★ Worked on the Financial Services domain for Abu Dhabi Bank (Capgemini Client).",
           text02:
-            "★ Built responsive and global component UIs using a modern technology stack like Reactjs, Typescript & other third-party libraries like Material UI, Formick, Yup etc.",
+            "★ Built responsive and global components UIs using a modern technology stack like ReactJS, Typescript & other third-party libraries like Material UI, Formick, Yup, etc.",
           text03:
-            "★ Performed bug fixes, writing test cases, Identifying innovative ideas and proof of concepts according to project requirements.",
+            "★ Performed bug fixes, writing test cases, Identifying innovative ideas & proof of concepts as per need. ",
           text04:
-            "★ Collaborated and work hand-in-hand with backend, QA, design and product colleagues to support projects during all phases of delivery.",
+            "★ Collaborated and work hand-in-hand with backend, QA, design and product colleagues to support the projects during all phases of delivery.",
         },
 
         {
@@ -53,15 +53,15 @@ class Specilizing extends Component {
           title: "Role- Software Developer (Solution Engineer)",
           date: "Date: May 2020 - August 2020",
           text01:
-            "★ Developed 3+ Front End apps to analyze and process data for different clients",
+            "★ Developed 3+ Front-End apps to analyze and process data for different clients.",
           text02:
             "★ Ensure applications security and ability to interact with multiple APIs.",
           text03:
-            "★ Built responsive and global component UIs using a modern technology stack like Reactjs, Next JS & other third-party libraries like Material UI, Formick, Yup etc",
+            "★ Built responsive and global components UIs using a modern technology stack like React JS , Next JS & other third-party libraries like Material UI, Formick, Yup, etc.",
           text04:
-            '★ Developed and implemented a standalone data extraction "agent" and main-application features for a financial product.',
+            '★ Developed and implemented a standalone data extraction "agent" and main-application features for the financial product.',
           text05:
-            "★ Work closely with app development team including project and product manager, developers, and QA to determine problems, testing methods, and best practices",
+            "★ Work closely with app development team including the project and product manager, developers, and QA to determine problems, testing methods, and best practices.",
         },
         {
           id: 4,
@@ -73,16 +73,13 @@ class Specilizing extends Component {
           title: "Role- Trainee (Software Engineer)",
           date: "Date: August 2019 - May 2020",
           text01:
-            "★ Developed 3+ Front End apps to analyze and process data for different clients",
-          text02:
-            "★ Ensure applications security and ability to interact with multiple APIs.",
-          text03:
-            "★ Built responsive and global component UIs using a modern technology stack like Reactjs, Next JS & other third-party libraries like Material UI, Formick, Yup etc",
-          text04:
-            '★ Developed and implemented a standalone data extraction "agent" and main-application features for a financial product.',
-          text05:
-            "★ Work closely with app development team including project and product manager, developers, and QA to determine problems, testing methods, and best practices",
-        },
+          "★ Developed front-end applications across various platforms using latest industry-adopted technologies and frameworks with React, Redux.",
+        text02:
+          "★ Played a key role in the development, improvement, and operation of web-based software.",
+        text03:
+          "★ Gained hands-on expertise in Bootstrap, Foundation as well as front-end presentation frameworks web application frameworks.",
+        text04:
+          "★ Conducted unit and load testing for high profile customer, which reduced system failure rate by 70%.", },
         {
           id: 5,
           iconbox:
@@ -90,16 +87,16 @@ class Specilizing extends Component {
           iconcolor: "iconbox-icon color3",
           spanicon: "icon-brand",
           companyTitle: "Nagravision India Pvt Ltd",
-          title: "Role- Trainee",
+          title: "Role- Trainee (Tester)",
           date: "Date: July 2018 - July 2019",
           text01:
-            "★ Developed full-stack applications across various platforms using latest industry-adopted technologies and frameworks like React, Redux, Node Js with Mongo DB. ",
+            "★ Testing front-end Application and writing JavaScript Test case for Automated testing in Jenkins.",
           text02:
-            "★ Played a key role in the development, improvement, and operation of web-based software ",
+            "★ Increased the coverage of Test case from 15% to 85% , with adding over 350+ Test cases.",
           text03:
-            "★ Gained hands-on expertise in Bootstrap, Foundation, Express and similar back-end Node.js as well as front-end presentation frameworks web application frameworks ",
+            "★ Worked parallelly with front-end Developers and backend developers to know the client requirements and comping up with appropriate test strategies.",
           text04:
-            "★ Conducted unit and load testing for high profile customer facing applications, which reduced system failure rate by 70%",
+            "★ Demonstrated Testing plans, Demos. Documented all type of testing functionality and other depending on the need.",
         },
       ],
     };
